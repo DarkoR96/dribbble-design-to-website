0bda90740ad12e0bc5e71af17cdd6ee994d0b2c9
DarkoR96 <35816364+DarkoR96@users.noreply.github.com>
2018-01-25 20:56:54 +0100

    Add files via upload

---
+++ jqueryCustom.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+    
+	$('.navbar-toggle').click(function() {
+		$('#navmin').toggleClass('navbar-toggle2');
+		$('#navigation').toggleClass('widthcng');
+
+});
+
+});
